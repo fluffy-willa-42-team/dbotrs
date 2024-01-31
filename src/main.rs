75a2ba213ce2d283f89ad497a9b0056261924e2f
Fluffy_Kaiju <contact@dreemcloud.net>
2024-01-31 20:24:47 +0100

    Add first main

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use std::env;
+
+use serenity::async_trait;
+use serenity::gateway::ActivityData;
+use serenity::prelude::*;
+use serenity::model::channel::Message;
+use serenity::framework::standard::macros::{command, group};
+use serenity::framework::standard::{StandardFramework, Configuration, CommandResult};
+
+#[group]
+#[commands(ping)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, ctx: Context, ready: serenity::model::gateway::Ready) {
+        ctx.set_activity(Some(ActivityData::playing("H- hiiii~~")));
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let framework = StandardFramework::new().group(&GENERAL_GROUP);
+    framework.configure(Configuration::new().prefix("~")); // set the bot's prefix to "~"
+
+    // Login with a bot token from the environment
+    let token = env::var("DISCORD_TOKEN").expect("token");
+    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(token, intents)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "P-pOwOng! ").await?;
+	msg.react(ctx, '👉').await?;
+	msg.react(ctx, '👈').await?;
+
+    Ok(())
+}
