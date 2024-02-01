319b4d354bbc6af1e450b84dbff01056fc4624cc
Fluffy_Kaiju <contact@dreemcloud.net>
2024-02-01 22:14:21 +0000

    Add dependencies and implement dadjoke command

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::env;
 
+use reqwest::header::ACCEPT;
+use reqwest::Response;
+use serde_derive::Deserialize;
 use serenity::async_trait;
 use serenity::gateway::ActivityData;
 use serenity::prelude::*;
@@ -8,8 +11,12 @@ use serenity::framework::standard::macros::{command, group};
 use serenity::framework::standard::{StandardFramework, Configuration, CommandResult};
 
 #[group]
-#[commands(ping)]
-#[commands(date)]
+#[commands(
+    ping,
+    date,
+    dadjoke,
+    // bus
+)]
 struct General;
 
 struct Handler;
@@ -18,7 +25,7 @@ struct Handler;
 impl EventHandler for Handler {
     async fn ready(&self, ctx: Context, ready: serenity::model::gateway::Ready) {
 
-        ctx.set_activity(Some(ActivityData::playing("H- hiiii~~")));
+        ctx.set_activity(Some(ActivityData::playing("H- hiiii~~ v0.0.1")));
 
         println!("{} is connected!", ready.user.name);
     }
@@ -27,7 +34,7 @@ impl EventHandler for Handler {
 #[tokio::main]
 async fn main() {
     let framework = StandardFramework::new().group(&GENERAL_GROUP);
-    framework.configure(Configuration::new().prefix("~")); // set the bot's prefix to "~"
+    framework.configure(Configuration::new().prefix("!")); // set the bot's prefix to "~"
 
     // Login with a bot token from the environment
     let token = env::var("DISCORD_TOKEN").expect("token");
@@ -62,3 +69,66 @@ async fn date(ctx: &Context, msg: &Message) -> CommandResult {
     print!("The current date and time is: {}", date);
     Ok(())
 }
+
+#[derive(Deserialize)]
+struct JokeResponse {
+    // id: String,
+    joke: String,
+    // status: u16,
+}
+
+async fn dadjoke_api() -> Result<String, reqwest::Error> {
+    let client = reqwest::Client::new();
+
+    let res: Response = client
+        .get("https://icanhazdadjoke.com/ ")
+        .header(ACCEPT, "application/json")
+        .send()
+        .await?;
+
+    println!("Status: {}", res.status());
+
+    let joke: JokeResponse = match res.json().await {
+        Ok(joke) => joke,
+        Err(_) => JokeResponse {
+            // id: String::new(),
+            joke: "Error".to_string(),
+            // status: 0,
+        },
+    };
+
+    return Ok(joke.joke);
+}
+
+#[command]
+async fn dadjoke(ctx: &Context, msg: &Message) -> CommandResult {
+
+    let joke = dadjoke_api().await?;
+
+    println!("Joke:\n{}", joke);
+
+    msg.reply(ctx, joke).await?;
+    msg.react(ctx, '😀').await?;
+    msg.react(ctx, '😃').await?;
+    msg.react(ctx, '😄').await?;
+    msg.react(ctx, '😁').await?;
+    msg.react(ctx, '😆').await?;
+    msg.react(ctx, '😅').await?;
+    msg.react(ctx, '😂').await?;
+    msg.react(ctx, '🤣').await?;
+    msg.react(ctx, '🥲').await?;
+    msg.react(ctx, '🥹').await?;
+
+    Ok(())
+}
+
+
+//TODO print joke with this api https://dragon.best/api/brexit_bus.jpg?text=lol
+// #[command]
+// async fn bus(ctx: &Context, msg: &Message) -> CommandResult {
+//     let joke = dadjoke_api().await?;
+
+//     println!("Joke:\n{}", joke);
+
+
+// }
